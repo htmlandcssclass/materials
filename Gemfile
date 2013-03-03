69645e07878d7f583a95ced9b7b08c1fbabf077b
Sean Durham <sean@sdurham.net>
2013-03-03 09:23:59 -0800

    Configure middleman-deploy.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem "middleman", "~>3.0.0"
+gem "middleman-deploy", "0.0.10"
 
 group :development, :test do
   gem 'pry'
--- config.rb
+++ config.rb
@@ -1,53 +1,10 @@
-###
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :navigator do
-#   page "*"
-# end
-
-# Proxy (fake) files
-# data.helloworld.exercises.each do |e|
-#   page "/helloworld/#{e}/index.html", :proxy => "/helloworld/helloworld.html", :ignore => true do
-#     @title = e.titlecase
-#   end
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+activate :deploy do |deploy|
+  deploy.method = :ftp
+  deploy.host = IO.read(File.dirname(__FILE__)+'/.secrets/host.txt')
+  deploy.user = IO.read(File.dirname(__FILE__)+'/.secrets/user.txt')
+  deploy.password = IO.read(File.dirname(__FILE__)+'/.secrets/password.txt')
+  deploy.path = ""
+end
 
 helpers do
   def title
@@ -78,26 +35,3 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
-end
